cf86256ad5d50110b2d4cb2faa04142b132fce7c
BaptFr <baptistesalazar@gmail.com>
2024-05-21 16:32:07 +0200

    bug test2

--- src/index.jsx
+++ src/index.jsx
@@ -7,14 +7,15 @@ import NotFound from './pages/error-page.jsx';
 import Logement from './pages/logement-page.jsx';
 import Layout from './components/layout/layout.jsx';
 
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Layout>
           <Routes>
-            <Route path="/kasa/" element={<Home />} />
-            <Route path="/kasa/about" element={<About />} />
-            <Route path="/kasa/logement/:id" element={<Logement />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/logement/:id" element={<Logement />} />
             <Route path="*" element={<NotFound />} />   
           </Routes>
       </Layout>
