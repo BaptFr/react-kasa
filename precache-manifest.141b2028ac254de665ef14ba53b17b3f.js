7aaa31d8072e3272a0f892e06e40e8d190daee33
BaptFr <baptistesalazar@gmail.com>
2024-05-21 16:56:15 +0200

    Updates

---
+++ precache-manifest.141b2028ac254de665ef14ba53b17b3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418c51a8ca369f65b93779adc19bef43",
+    "url": "/react-kasa/index.html"
+  },
+  {
+    "revision": "1b3134cf8cf933dffa13",
+    "url": "/react-kasa/static/css/main.b3312107.chunk.css"
+  },
+  {
+    "revision": "a7dde20dad57bb01aedf",
+    "url": "/react-kasa/static/js/2.4819fb91.chunk.js"
+  },
+  {
+    "revision": "99562942502e41af72fc73d84afe4332",
+    "url": "/react-kasa/static/js/2.4819fb91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b3134cf8cf933dffa13",
+    "url": "/react-kasa/static/js/main.15b1d18e.chunk.js"
+  },
+  {
+    "revision": "7d215af3b551e430e07e",
+    "url": "/react-kasa/static/js/runtime-main.dcfd6d18.js"
+  },
+  {
+    "revision": "40349702c69af9d1b661fdc71d2b6a94",
+    "url": "/react-kasa/static/media/about-banner-pic.40349702.jpg"
+  },
+  {
+    "revision": "6d416d00581d30d8bf9ab591b8c0a497",
+    "url": "/react-kasa/static/media/home-banner-pic.6d416d00.jpg"
+  }
+]);
